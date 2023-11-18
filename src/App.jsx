4060a97aa681dbed0c2fdcfa35d7454f401e5720
Diego Gularte <diegohfg@gmail.com>
2023-11-18 17:38:10 -0300

    hooks05 passando funcao como parametro para child

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import UseState02 from './components/UseState02';
 import UseState01 from './components/UseState01';
 import UseState03 from './components/UseState03';
 import UseState04 from './components/UseState04';
+import UseState05 from './components/useState05';
+
 export default () => {
   return (
     <>
       <h1>React</h1>
-      <teste />
       <UseState01 />
       <UseState02 />
       {/*<UseState03/>*/}
       <UseState04 />
+      <UseState05 />
+
 
 
     </>
